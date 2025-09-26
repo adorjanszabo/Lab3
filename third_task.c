bfc2991dd8c58270f4fd1b887b6db4fdb9cd59a5
adorjanszabo <adorjan.szabo.hu@gmail.com>
2025-09-27 00:11:47 +0200

    Update third_task.c

--- third_task.c
+++ third_task.c
@@ -14,7 +14,15 @@ Hint: if you have used nested loops, try to simplify your solution! A single loo
 
 int main()
 {
+    double e =1.0;
+    double fact =1.0;
 
+    for(int i = 1; i<=20; i++){
+        fact *= i; 
+        e += 1.0/fact;}
 
+    printf("approx e = %.15f\n", e);
+    
+    return 0;
 
 }
